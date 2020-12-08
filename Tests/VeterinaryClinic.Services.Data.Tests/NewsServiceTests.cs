ec37bd41dd49bc8896cd5b1a44409bbd5360916d
Megi <megi.d0790@gmail.com>
2020-12-08 13:40:29 +0200

    Add tests for services and news services

---
+++ Tests/VeterinaryClinic.Services.Data.Tests/NewsServiceTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinaryClinic.Data;
+using VeterinaryClinic.Data.Common.Repositories;
+using VeterinaryClinic.Data.Models;
+using VeterinaryClinic.Data.Repositories;
+using VeterinaryClinic.Services.Mapping;
+using VeterinaryClinic.Web.ViewModels.News;
+using Xunit;
+
+namespace VeterinaryClinic.Services.Data.Tests
+{
+    public class NewsServiceTests
+    {
+        [Fact]
+        public void GetCountShouldReturnCorrectNumberOfNews()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var newsRepository = new EfDeletableEntityRepository<News>(new ApplicationDbContext(options.Options));
+
+            var newsService = new NewsService(newsRepository);
+
+            var count = newsService.GetCount();
+
+            Assert.Equal(0,count);
+        }
+
+        [Fact]
+        public async Task GetByIdShouldReturnCorrectEntity()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var repository = new EfDeletableEntityRepository<News>(new ApplicationDbContext(options.Options));
+            await repository.AddAsync(new News { Title = "test", Content="testContent", Summary="testSummary" });
+            await repository.SaveChangesAsync();
+            var newsService = new NewsService(repository);
+            AutoMapperConfig.RegisterMappings(typeof(NewsDetailsViewModel).Assembly);
+            var news = newsService.GetById<NewsDetailsViewModel>(1);
+
+            Assert.Equal("test", news.Title);
+            Assert.Equal("testContent", news.Content);
+            Assert.Null(news.ImageUrl);
+        }
+
+        [Fact]
+        public async Task GetAllForAPageShouldReturnCorrectEntities()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var repository = new EfDeletableEntityRepository<News>(new ApplicationDbContext(options.Options));
+            await repository.AddAsync(new News { Title = "test1", Content = "testContent1", Summary = "testSummary1" });
+            await repository.AddAsync(new News { Title = "test2", Content = "testContent2", Summary = "testSummary2" });
+            await repository.AddAsync(new News { Title = "test3", Content = "testContent3", Summary = "testSummary3" });
+            await repository.AddAsync(new News { Title = "test4", Content = "testContent4", Summary = "testSummary4" });
+            await repository.AddAsync(new News { Title = "test5", Content = "testContent5", Summary = "testSummary5" });
+            await repository.AddAsync(new News { Title = "test6", Content = "testContent6", Summary = "testSummary6" });
+            await repository.AddAsync(new News { Title = "test7", Content = "testContent7", Summary = "testSummary7" });
+            await repository.SaveChangesAsync();
+            var newsService = new NewsService(repository);
+            AutoMapperConfig.RegisterMappings(typeof(NewsDetailsViewModel).Assembly);
+            var news = newsService.GetAllForAPage<NewsDetailsViewModel>(1).ToList();
+            
+            for (int i = 1; i <= news.Count(); i++)
+            {
+                Assert.Equal("test" + i, news[i-1].Title);
+                Assert.Equal("testContent" + i, news[i-1].Content);
+                Assert.Null(news[i-1].ImageUrl);
+            }
+        }
+
+        [Theory]
+        [InlineData(1,6)]
+        [InlineData(2,1)]
+        [InlineData(3,0)]
+        public async Task GetAllForAPageShouldReturnCorrectCount(int page,int expectedCount)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var repository = new EfDeletableEntityRepository<News>(new ApplicationDbContext(options.Options));
+            await repository.AddAsync(new News { Title = "test1", Content = "testContent1", Summary = "testSummary1" });
+            await repository.AddAsync(new News { Title = "test2", Content = "testContent2", Summary = "testSummary2" });
+            await repository.AddAsync(new News { Title = "test3", Content = "testContent3", Summary = "testSummary3" });
+            await repository.AddAsync(new News { Title = "test4", Content = "testContent4", Summary = "testSummary4" });
+            await repository.AddAsync(new News { Title = "test5", Content = "testContent5", Summary = "testSummary5" });
+            await repository.AddAsync(new News { Title = "test6", Content = "testContent6", Summary = "testSummary6" });
+            await repository.AddAsync(new News { Title = "test7", Content = "testContent7", Summary = "testSummary7" });
+            await repository.SaveChangesAsync();
+            var newsService = new NewsService(repository);
+            AutoMapperConfig.RegisterMappings(typeof(NewsDetailsViewModel).Assembly);
+            var news = newsService.GetAllForAPage<NewsDetailsViewModel>(page).ToList();
+
+            var actualCount = news.Count();
+
+            Assert.Equal(expectedCount, actualCount);
+
+        }
+
+    }
+}
---
+++ Tests/VeterinaryClinic.Services.Data.Tests/ServicesServiceTests.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinaryClinic.Data;
+using VeterinaryClinic.Data.Models;
+using VeterinaryClinic.Data.Repositories;
+using VeterinaryClinic.Services.Mapping;
+using VeterinaryClinic.Web.ViewModels.Services;
+using VeterinaryClinic.Web.ViewModels.Vets;
+using Xunit;
+
+namespace VeterinaryClinic.Services.Data.Tests
+{
+    public class ServicesServiceTests
+    {
+        [Fact]
+        public async Task GetNameByIdShouldReturnCorrectName()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+
+            await servicesRepository.AddAsync(new Service { Name="test",Description="testDesc" });
+            await servicesRepository.SaveChangesAsync();
+
+            var servicesService = new ServicesService(servicesRepository,vetsServicesRepository);
+
+            var actualName = servicesService.GetNameById(1);
+
+            Assert.Equal("test", actualName);
+        }
+
+        [Fact]
+        public async Task GetByIdShouldReturnCorrectEntity()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+
+            await servicesRepository.AddAsync(new Service { Name = "test", Description = "testDesc" });
+            await servicesRepository.SaveChangesAsync();
+
+            var servicesService = new ServicesService(servicesRepository, vetsServicesRepository);
+
+            AutoMapperConfig.RegisterMappings(typeof(ServiceDetailsViewModel).Assembly);
+            var service = servicesService.GetById<ServiceDetailsViewModel>(1);
+
+            Assert.Equal("test", service.Name);
+            Assert.Equal("testDesc", service.Description);
+            Assert.Null(service.ImageUrl);
+        }
+
+        [Fact]
+        public async Task GetAllShouldReturnCorrectEntities()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+
+            await servicesRepository.AddAsync(new Service { Name = "test1", Description = "testDesc1" });
+            await servicesRepository.AddAsync(new Service { Name = "test2", Description = "testDesc2" });
+            await servicesRepository.AddAsync(new Service { Name = "test3", Description = "testDesc3" });
+            await servicesRepository.SaveChangesAsync();
+
+            var servicesService = new ServicesService(servicesRepository, vetsServicesRepository);
+
+            AutoMapperConfig.RegisterMappings(typeof(ServiceDetailsViewModel).Assembly);
+            var services = servicesService.GetAll<ServiceDetailsViewModel>().ToList();
+
+            for (int i = 1; i <= services.Count(); i++)
+            {
+                Assert.Equal("test" + i, services[i-1].Name);
+                Assert.Equal("testDesc" + i, services[i-1].Description);
+                Assert.Null(services[i-1].ImageUrl);
+            }
+        }
+
+        [Fact]
+        public async Task GetAllShouldReturnCorrectCount()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+
+            await servicesRepository.AddAsync(new Service { Name = "test1", Description = "testDesc1" });
+            await servicesRepository.AddAsync(new Service { Name = "test2", Description = "testDesc2" });
+            await servicesRepository.AddAsync(new Service { Name = "test3", Description = "testDesc3" });
+            await servicesRepository.SaveChangesAsync();
+
+            var servicesService = new ServicesService(servicesRepository, vetsServicesRepository);
+
+            AutoMapperConfig.RegisterMappings(typeof(ServiceDetailsViewModel).Assembly);
+            var services = servicesService.GetAll<ServiceDetailsViewModel>().ToList();
+
+            var expectedCount = 3;
+            var actualCount = services.Count();
+
+            Assert.Equal(expectedCount, actualCount);
+
+        }
+
+        [Fact]
+        public async Task GetAllServicesWhichAVetDoesNotHaveShouldReturnCorrectEntity()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+            var servicesService = new ServicesService(servicesRepository, vetsServicesRepository);
+            
+            await servicesRepository.AddAsync(new Service { Name = "test1", Description = "testDesc1" });
+            await servicesRepository.AddAsync(new Service { Name = "test2", Description = "testDesc2" });
+            await servicesRepository.AddAsync(new Service { Name = "test3", Description = "testDesc3" });
+            await servicesRepository.SaveChangesAsync();
+
+            await vetsServicesRepository.AddAsync(new VetsServices { ServiceId = 1.ToString(), VetId = "dspps-4592-sjfis-sjs" });
+            await vetsServicesRepository.SaveChangesAsync();
+
+            AutoMapperConfig.RegisterMappings(typeof(ServiceViewModel).Assembly);
+
+            var services = servicesService.GetAllServicesWhichAVetDoesNotHave<ServiceViewModel>("dspps-4592-sjfis-sjs").ToList();
+
+            for (int i = 1; i <= services.Count(); i++)
+            {
+                Assert.Equal(i+1, services[i - 1].Id);
+            }
+
+        }
+
+        [Fact]
+        public async Task GetAllServicesWhichAVetDoesNotHaveShouldReturnCorrectCount()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+            var servicesService = new ServicesService(servicesRepository, vetsServicesRepository);
+
+            await servicesRepository.AddAsync(new Service { Name = "test1", Description = "testDesc1" });
+            await servicesRepository.AddAsync(new Service { Name = "test2", Description = "testDesc2" });
+            await servicesRepository.AddAsync(new Service { Name = "test3", Description = "testDesc3" });
+            await servicesRepository.SaveChangesAsync();
+
+            await vetsServicesRepository.AddAsync(new VetsServices { ServiceId = 1.ToString(), VetId = "dspps-4592-sjfis-sjs" });
+            await vetsServicesRepository.SaveChangesAsync();
+
+            AutoMapperConfig.RegisterMappings(typeof(ServiceViewModel).Assembly);
+
+            var services = servicesService.GetAllServicesWhichAVetDoesNotHave<ServiceViewModel>("dspps-4592-sjfis-sjs").ToList();
+
+            Assert.Equal(2, services.Count);
+        }
+
+        [Fact]
+        public async Task AddServiceToVetShouldAddCorrectly()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var servicesRepository = new EfDeletableEntityRepository<Service>(new ApplicationDbContext(options.Options));
+            var vetsServicesRepository = new EfDeletableEntityRepository<VetsServices>(new ApplicationDbContext(options.Options));
+            var servicesService = new ServicesService(servicesRepository, vetsServicesRepository);
+
+            await servicesRepository.AddAsync(new Service { Name = "test1", Description = "testDesc1" });
+            await servicesRepository.AddAsync(new Service { Name = "test2", Description = "testDesc2" });
+            await servicesRepository.AddAsync(new Service { Name = "test3", Description = "testDesc3" });
+            await servicesRepository.SaveChangesAsync();
+
+            await vetsServicesRepository.AddAsync(new VetsServices { ServiceId = 1.ToString(), VetId = "dspps-4592-sjfis-sjs" });
+            await vetsServicesRepository.SaveChangesAsync();
+
+            AutoMapperConfig.RegisterMappings(typeof(ServiceViewModel).Assembly);
+
+            await servicesService.AddServiceToVet(new AddServiceToVetInputModel { VetId = "dspps-4592-sjfis-sjs", Services = new List<int> { 1, 2 } });
+
+            var expectedCount = 2;
+            var actualCount = vetsServicesRepository.All().Where(x => x.VetId == "dspps-4592-sjfis-sjs").Count();
+
+            Assert.Equal(expectedCount, actualCount);
+        }
+
+
+
+    }
+}
