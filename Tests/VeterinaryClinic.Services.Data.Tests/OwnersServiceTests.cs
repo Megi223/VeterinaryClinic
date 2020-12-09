58d091915f407fa90053f01a7b445db4f8abea16
Megi <megi.d0790@gmail.com>
2020-12-09 20:29:13 +0200

    add tests for owners service and petsMedicationsService

---
+++ Tests/VeterinaryClinic.Services.Data.Tests/OwnersServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinaryClinic.Data;
+using VeterinaryClinic.Data.Models;
+using VeterinaryClinic.Data.Repositories;
+using Xunit;
+
+namespace VeterinaryClinic.Services.Data.Tests
+{
+    public class OwnersServiceTests
+    {
+        [Fact]
+        public async Task CreateOwnerAsyncShouldAddToDbCorrectEntity()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var ownersRepository = new EfDeletableEntityRepository<Owner>(new ApplicationDbContext(options.Options));
+            var reviewsRepository = new EfDeletableEntityRepository<Review>(new ApplicationDbContext(options.Options));
+
+            var service = new OwnersService(ownersRepository, reviewsRepository);
+
+            await service.CreateOwnerAsync(new ApplicationUser(),"firstName","lastName","someUrl","city");
+
+            var count = ownersRepository.All().Count();
+
+            Assert.Equal(1, count);
+            Assert.Equal("firstName", ownersRepository.All().First().FirstName);
+            Assert.Equal("lastName", ownersRepository.All().First().LastName);
+            Assert.Equal("someUrl", ownersRepository.All().First().ProfilePicture); 
+            Assert.Equal("city", ownersRepository.All().First().City);
+        }
+
+        [Fact]
+        public async Task GetOwnerIdShouldReturnCorrectId()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var ownersRepository = new EfDeletableEntityRepository<Owner>(new ApplicationDbContext(options.Options));
+            var reviewsRepository = new EfDeletableEntityRepository<Review>(new ApplicationDbContext(options.Options));
+
+            var service = new OwnersService(ownersRepository, reviewsRepository);
+
+            await ownersRepository.AddAsync(new Owner { Id="testOwnerId123",UserId = "testUserId123", FirstName = "firstName", LastName = "lastName", ProfilePicture = "someUrl", City = "city" });
+            await ownersRepository.SaveChangesAsync();
+
+            var actualOwnerId = service.GetOwnerId("testUserId123");
+
+            Assert.Equal("testOwnerId123", actualOwnerId);
+        }
+
+        [Fact]
+        public async Task WriteReviewAsyncShouldAddToDb()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var ownersRepository = new EfDeletableEntityRepository<Owner>(new ApplicationDbContext(options.Options));
+            var reviewsRepository = new EfDeletableEntityRepository<Review>(new ApplicationDbContext(options.Options));
+
+            var service = new OwnersService(ownersRepository, reviewsRepository);
+
+            await service.WriteReviewAsync("testOwnerId123", new Web.ViewModels.Reviews.ReviewInputModel { Content = "testContent" });
+
+            var count = reviewsRepository.All().Count();
+
+            Assert.Equal(1, count);
+        }
+
+    }
+}
---
+++ Tests/VeterinaryClinic.Services.Data.Tests/PetsMedicationsServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinaryClinic.Data;
+using VeterinaryClinic.Data.Models;
+using VeterinaryClinic.Data.Repositories;
+using VeterinaryClinic.Web.ViewModels.Appointments;
+using Xunit;
+
+namespace VeterinaryClinic.Services.Data.Tests
+{
+    public class PetsMedicationsServiceTests
+    {
+        [Fact]
+        public async Task PrescribeMedicationAsyncShouldAddToBothRepositories()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var medicationsRepository = new EfDeletableEntityRepository<Medication>(new ApplicationDbContext(options.Options));
+            var petsMedicationsRepository = new EfDeletableEntityRepository<PetsMedications>(new ApplicationDbContext(options.Options));
+
+            var service = new PetsMedicationsService(petsMedicationsRepository, medicationsRepository);
+
+            await service.PrescribeMedicationAsync(new PrescribeMedicationViewModel { PetId="testPetId123",Medications=new List<PetsMedicationsInputModel> { new PetsMedicationsInputModel { MedicationName="testMedicationName", MedicationNumberOfDosesPerServing="5"}, new PetsMedicationsInputModel { MedicationName="testMedicationName2",MedicationNumberOfDosesPerServing="3"} } });
+
+            var medicationsRepositoryActualCount = medicationsRepository.All().Count();
+            var petsMedicationRepositoryActualCount = medicationsRepository.All().Count();
+
+            Assert.Equal(2, medicationsRepositoryActualCount);
+            Assert.Equal(2, petsMedicationRepositoryActualCount);
+        }
+
+        [Fact]
+        public async Task EndMedicationAsyncShouldDeleteFromBothRepositories()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var medicationsRepository = new EfDeletableEntityRepository<Medication>(new ApplicationDbContext(options.Options));
+            var petsMedicationsRepository = new EfDeletableEntityRepository<PetsMedications>(new ApplicationDbContext(options.Options));
+
+            var service = new PetsMedicationsService(petsMedicationsRepository, medicationsRepository);
+
+            await medicationsRepository.AddAsync(new Medication { Id = 1, Name = "testMedicationName", NumberOfDosesPerServing = "5" });
+            await medicationsRepository.AddAsync(new Medication { Id = 2, Name = "testMedicationName2", NumberOfDosesPerServing = "3" });
+            await medicationsRepository.SaveChangesAsync();
+
+            await petsMedicationsRepository.AddAsync(new PetsMedications { PetId = "testPetId123", MedicationId = 1 });
+            await petsMedicationsRepository.AddAsync(new PetsMedications { PetId = "testPetId123", MedicationId = 2 });
+            await petsMedicationsRepository.SaveChangesAsync();
+
+            await service.EndMedicationAsync(1);
+
+            var medicationsRepositoryActualCount = medicationsRepository.All().Count();
+            var petsMedicationRepositoryActualCount = medicationsRepository.All().Count();
+
+            Assert.Equal(1, medicationsRepositoryActualCount);
+            Assert.Equal(1, petsMedicationRepositoryActualCount);
+        }
+
+    }
+}
